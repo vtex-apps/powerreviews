3aa48c08dbfa9a39effe046c863819b8b25dff16
Breno Calazans <brenocalazans@gmail.com>
2019-07-11 20:11:07 -0300

    Add safeguard if product is not loaded yet

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Add safeguard if product is not loaded yet.
+
 ## [1.0.7] - 2019-07-11
 
 ### Fixed
--- react/RatingSummary.tsx
+++ react/RatingSummary.tsx
@@ -22,13 +22,24 @@ const RatingSummary: FunctionComponent<
   const { product } = useContext(ProductContext)
 
   const writeReviewLink =
-    props.data && props.data.getConfig
+    props.data && props.data.getConfig && product
       ? `/new-review?pr_page_id=${
           product[props.data.getConfig.uniqueId]
         }&pr_merchant_id=${props.data.getConfig.merchantId}&pr_api_key=${
           props.data.getConfig.appKey
         }&pr_merchant_group_id=${props.data.getConfig.merchantGroupId}`
       : ''
+
+  if (!product) {
+    return (
+      <Summary
+        loading
+        writeReviewLink={writeReviewLink}
+        rating={0}
+        numberOfReviews={0}
+      />
+    )
+  }
 
   return (
     <Query
