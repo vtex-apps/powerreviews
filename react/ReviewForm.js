643d454f6b96eb23ce22550deb3f0d477d91f66d
Breno Calazans <brenocalazans@gmail.com>
2019-07-30 10:35:14 -0300

    Simplify code of ReviewForm

--- react/ReviewForm.js
+++ react/ReviewForm.js
@@ -10,51 +10,42 @@ const ReviewForm = props => {
   } = useRuntime()
   const [loaded, setLoaded] = useState(false)
   const [pageId, setPageId] = useState(null)
-  const [appKey, setAppKey] = useState(null)
-  const [merchantId, setMerchantId] = useState(null)
-  const [merchantGroupId, setMerchantGroupId] = useState(null)
+  const { appKey, merchantId, merchantGroupId } = props.data.getConfig || {}
 
   useEffect(() => {
     if (!props.data.loading) {
       var script = document.createElement('script')
       script.onload = function() {
         setPageId(query.pr_page_id)
-        setMerchantGroupId(query.pr_merchant_group_id)
-        setAppKey(props.data.getConfig.appKey)
-        setMerchantId(props.data.getConfig.merchantId)
         setLoaded(true)
       }
       script.src = 'https://ui.powerreviews.com/stable/4.0/ui.js'
       document.body.appendChild(script)
     }
   }, [
-    props.data.getConfig.appKey,
-    props.data.getConfig.merchantId,
+    appKey,
+    merchantId,
+    merchantGroupId,
     props.data.loading,
-    query.pr_merchant_group_id,
     query.pr_page_id,
   ])
 
   useEffect(() => {
-    if (!window.POWERREVIEWS && appKey === null) {
-      // eslint-disable-next-line no-console
-      console.log('POWERREVIEWS NO')
-    } else {
-      // eslint-disable-next-line no-console
-      console.log('POWERREVIEWS SIM')
-
-      /* eslint-disable @typescript-eslint/camelcase */
-      window.POWERREVIEWS.display.render({
-        api_key: appKey,
-        locale: locale,
-        merchant_group_id: merchantGroupId,
-        merchant_id: merchantId,
-        page_id: pageId,
-        components: {
-          Write: 'pr-write',
-        },
-      })
+    if (!window.POWERREVIEWS || loaded === false) {
+      return
     }
+
+    /* eslint-disable @typescript-eslint/camelcase */
+    window.POWERREVIEWS.display.render({
+      api_key: appKey,
+      locale: locale,
+      merchant_group_id: merchantGroupId,
+      merchant_id: merchantId,
+      page_id: pageId,
+      components: {
+        Write: 'pr-write',
+      },
+    })
   }, [appKey, loaded, locale, merchantGroupId, merchantId, pageId])
 
   return <div id="pr-write"></div>
