73b86f9ce01d0ae1d794f87bf6233f89b3821d37
Breno Calazans <brenocalazans@gmail.com>
2019-07-30 10:34:45 -0300

    Create Stars component

--- react/RatingInline.tsx
+++ react/RatingInline.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useState, useEffect } from 'react'
 import { useProductSummary } from 'vtex.product-summary-context/ProductSummaryContext'
+import Stars from './components/Stars'
 import queryRatingSummary from './graphql/queries/queryRatingSummary.gql'
 import { withApollo } from 'react-apollo'
 
@@ -47,105 +48,9 @@ const RatingInline: FunctionComponent<RatingInlineProps> = props => {
     }
   }, [count, product, props.client])
 
-  return reviews.length ? (
+  return (
     <div className="review__rating mw8 center ph5">
-      <div className="review__rating--stars dib relative v-mid mr2">
-        <div className="review__rating--inactive nowrap">
-          {[0, 1, 2, 3, 4].map((_, i) => {
-            return i <= 3 ? (
-              <svg
-                className="mr2"
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={'#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            ) : (
-              <svg
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={'#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            )
-          })}
-        </div>
-        <div
-          className="review__rating--active nowrap overflow-hidden absolute top-0-s left-0-s"
-          style={{ width: average * 20 + '%' }}
-        >
-          {[0, 1, 2, 3, 4].map((_, i) => {
-            return i <= 3 ? (
-              <svg
-                className="mr2"
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={average > i ? '#fc0' : '#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            ) : (
-              <svg
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={average > i ? '#fc0' : '#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            )
-          })}
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div className="review__rating mw8 center ph5">
-      <div className="review__rating--stars dib relative v-mid mr2">
-        <div className="review__rating--inactive nowrap">
-          {[0, 1, 2, 3, 4].map((_, i) => {
-            return (
-              <svg
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="#eee"
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                ></path>
-              </svg>
-            )
-          })}
-        </div>
-      </div>
+      <Stars rating={reviews.length ? average : 0} />
     </div>
   )
 }
--- react/RatingSummary.tsx
+++ react/RatingSummary.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useContext } from 'react'
 import { FormattedMessage } from 'react-intl'
 import { ProductContext } from 'vtex.product-context'
+import Stars from './components/Stars'
 import queryRatingSummary from './graphql/queries/queryRatingSummary.gql'
 import getConfig from './graphql/getConfig.gql'
 import { withApollo, graphql, ChildProps, Query } from 'react-apollo'
@@ -82,81 +83,7 @@ const Summary: FunctionComponent<SummaryProps> = ({
 }) => {
   return (
     <div className="review__rating mw8 center mb5">
-      <div className="review__rating--stars dib relative v-mid mr4">
-        <div className="review__rating--inactive nowrap">
-          {[0, 1, 2, 3, 4].map((_, i) => {
-            return i <= 3 ? (
-              <svg
-                className="mr2"
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={'#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            ) : (
-              <svg
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={'#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            )
-          })}
-        </div>
-        <div
-          className="review__rating--active nowrap overflow-hidden absolute top-0-s left-0-s"
-          style={{ width: rating * 20 + '%' }}
-        >
-          {[0, 1, 2, 3, 4].map((_, i) => {
-            // let { average } = state;
-
-            return i <= 3 ? (
-              <svg
-                className="mr2"
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={rating > i ? '#fc0' : '#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            ) : (
-              <svg
-                key={i}
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill={rating > i ? '#fc0' : '#eee'}
-                viewBox="0 0 14.737 14"
-              >
-                <path
-                  d="M7.369,11.251,11.923,14,10.714,8.82l4.023-3.485-5.3-.449L7.369,0,5.3,4.885,0,5.335,4.023,8.82,2.815,14Z"
-                  transform="translate(0)"
-                />
-              </svg> // se o review.metrics.rating for 4, preenche 4 estrelas
-            )
-          })}
-        </div>
-      </div>
+      <Stars rating={rating} />
       <a
         href="#all-reviews"
         className="review__rating--average mr4 dib c-muted-2 t-body f6-s"
